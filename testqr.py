a88f5ccee4994bee0dca8381badcb1a1a5e62ecf
Adarsh Gupta <113763427+apneduniya@users.noreply.github.com>
2023-03-19 22:53:43 +0530

    Updated this code by adding proper comments, and few lines of code

--- testqr.py
+++ testqr.py
@@ -1,13 +1,19 @@
+# importing qrcode module
+
 import qrcode
-print("Генератор QR")
-print("Checking Compability")
-print("Govnosborkinosoft 2023")
-print("Enter text for Qr")
-text = input("Qr Text: ")
-print("enter filename")
-filename = input("Enter Filename: ") 
-img= qrcode.make(text)
-img.save(filename + ".png")
-print("Sucess")
-print("file" + filename + ".png" + "Created")
-print("Vk.com/cackemc")
+
+
+if __name__ == "__main__":
+  print("Генератор QR")
+  print("Checking Compability")
+  print("Govnosborkinosoft 2023")
+
+  text = input("Qr Text: ") # Enter the text you want to make the QR Code of..
+  filename = input("Enter Filename: ") # Enter the file name you want to saved after the QR Code is made
+
+  img = qrcode.make(text) # Making the QR Code via make() function of qrcode
+  img.save(filename + ".png") # Saving it
+
+  print("Success!!")
+  print("file" + filename + ".png" + "Created")
+  print("Vk.com/cackemc")
